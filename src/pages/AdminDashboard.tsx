c675a775bcaf5760f976022d5da1ef0dec066b92
Gogo2006 <richardlu.2016@gmail.com>
2025-09-18 13:35:43 -0400

    Starting new UI for next Maker Meetup presentation

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,26 +1,38 @@
 import { useContext } from "react";
 import { AppContext } from "./App";
 import styles from '../styles/adminDashboard.module.css';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 function AdminDashboard() {
     const { profile } = useContext(AppContext);
 
-
     return (
-        <div className={styles.container}>
-            <h1 className={styles.greeting}>Welcome, {profile!["first_name"]}!</h1>
-            <section className={styles["verify-student-creds-section"]}>
-                <h2 className={styles["section-heading"]}>Verify Student Credentials</h2>
-                <div className={styles["section-content"]}>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dignissimos praesentium temporibus aliquid laborum dolor. Deserunt autem animi vel unde quas vero. Reiciendis aliquam consequatur nisi enim itaque deleniti repudiandae.</p>
+        <>
+            <div className={styles.navbar}>
+                <div className={styles.tab}>
+                    <p className={styles["tab-name"]}>Dashboard</p>
+                    <div className={styles["hover-line"]}></div>
                 </div>
-            </section>
-            <section className={styles["resolve-student-reports-redirect"]}>
-                <h2>Resolve Reports from Students</h2>
-                <button type="button" className={styles["redirect-button"]}><ArrowForwardIcon className={styles["redirect-icon"]} /></button>
-            </section>
-        </div>
+                <div className={styles.tab}>
+                    <p className={styles["tab-name"]}>My Tasks</p>
+                    <div className={styles["hover-line"]}></div>
+                </div>
+                <div className={styles.tab}>
+                    <p className={styles["tab-name"]}>Usage Statistics</p>
+                    <div className={styles["hover-line"]}></div>
+                </div>
+            </div>
+            <div className={styles.container}>
+                <h1 className={styles.greeting}>Welcome back, {profile!["first_name"]}!</h1>
+                <div className={styles["main-content"]}>
+                    <section className={styles["latest-notifications"]}>
+                        <h3 className={styles["section-heading"]}>Latest Notifications</h3>
+                    </section>
+                    <section>
+                        <h3 className={styles["section-heading"]}>Design Lab Usage</h3>
+                    </section>
+                </div>
+            </div>
+        </>
     );
 }
 
